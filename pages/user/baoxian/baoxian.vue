5b75e5d7abe905627b088de214676408fdfaa9c1
Allen <4668460@qq.com>
2023-02-07 20:58:57 +0800

    update baoxian

--- pages/user/baoxian/baoxian.vue
+++ pages/user/baoxian/baoxian.vue
@@ -1,5 +1,5 @@
 <template>
-	<view class="page">
+	<view class="page bg-gray">
 		<view class="baoxian-nav">
 			<view class="baoxian-nav-item">
 				<text @click="toOrder">我的保单</text>
@@ -8,11 +8,37 @@
 				<text @click="toOrder">理赔报案</text>
 			</view>
 		</view>
-		<view class="page-warp" style="padding-top: 50px;">
-			<view class="detail-content">
-				保险产品详情图文介绍页
+		<view class="insure-list">
+			<view class="insure-card">
+				<image class="insure-img" src="/static/21.jpg"></image>
+				<view class="insure-card-body">
+					<text class="insure-title">保险产品详情图文介绍页</text>
+					<view class="insure-card-info">
+						<view class="insure-price">
+							<text class="insure-price-prefix">￥</text>
+							<text class="insure-price-num">99</text>
+							<text class="insure-price-text">起</text>
+						</view>
+						<t-button text="立即投保" block type="warning" shape="circle" @click="toForm"></t-button>
+					</view>
+				</view>
+			</view>
+			<view class="insure-card">
+				<image class="insure-img" src="/static/21.jpg"></image>
+				<view class="insure-card-body">
+					<text class="insure-title">保险产品详情图文介绍页</text>
+					<view class="insure-card-info">
+						<view class="insure-price">
+							<text class="insure-price-prefix">￥</text>
+							<text class="insure-price-num">99</text>
+							<text class="insure-price-text">起</text>
+						</view>
+						<t-button text="立即投保" block type="warning" shape="circle" @click="toForm"></t-button>
+					</view>
+				</view>
 			</view>
 		</view>
+		
 		<lun-footer>
 			<view class="footer-center-btn"><t-button text="立即投保" block type="warning" shape="circle" size="lg" @click="toForm"></t-button></view>
 		</lun-footer>
@@ -33,7 +59,7 @@
 			}
 		},
 		onLoad(e) {
-			setTimeout(()=>{
+			/* setTimeout(()=>{
 				uni.showModal({
 					title: '投保须知',
 					content: '声明：所有保险产品的销售、理赔等服务均有XXXX保险经纪公司提供。',
@@ -47,7 +73,7 @@
 						}
 					}
 				});
-			}, 500)
+			}, 500) */
 		},
 		methods: {
 			swichNav: function(e) {
@@ -88,6 +114,7 @@
 	justify-content: space-between;
 	height: 100rpx;
 	border-bottom: #e5e5e5 solid 1rpx;
+	background-color: #fff;
 }
 .baoxian-nav-item{
 	flex: 1;
@@ -95,5 +122,59 @@
 	flex-direction: row;
 	justify-content: center;
 	align-items: center;
+}
+.insure-list{
+	width: 750rpx;
+	display: flex;
+	flex-direction: column;
+	padding: 30rpx;
+}
+.insure-card{
+	margin-bottom: 30rpx;
+	background-color: #fff;
+	border-radius: 16rpx;
+}
+.insure-img{
+	width: 690rpx;
+	height: 388rpx;
+	border-radius: 16rpx;
+}
+.insure-card-body{
+	display: flex;
+	flex-direction: column;
+	padding: 20rpx;
+}
+.insure-title{
+	font-size: 32rpx;
+	margin-bottom: 20rpx;
+}
+.insure-card-info{
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+}
+.insure-price{
+	font-size: 36rpx;
+	font-weight: bold;
+}
+.insure-price{
+	display: flex;
+	flex-direction: row;
+	align-items: flex-end;
+}
+.insure-price-prefix{
+	font-size: 28rpx;
+	color: #F01625;
+}
+.insure-price-num{
+	font-size: 38rpx;
+	color: #F01625;
+	font-weight: 700;
+	line-height: 1;
+}
+.insure-price-text{
+	font-size: 24rpx;
+	color: #333;
 }
 </style>
